0c53eaf99610bf5b7fc23ac3f9cd82081ade2126
Angela Lee <rubii.kun@gmail.com>
2019-02-08 11:19:35 -0800

    Add environment port detection

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
