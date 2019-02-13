98c7a48fd8f9d183a0571f59428214feed496ef3
Angela Lee <rubii.kun@gmail.com>
2019-02-12 20:03:30 -0800

    Use styled components on App
    Current rendering does not display. Will fix passing in state
    to render each element.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import { dummyData } from '../../../data';
+
+const Image = styled.div`
+  width: 100%;
+  height: 100%;
+  background-image: url(https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg);
+  background-size: 100% 100%;
+`;
+
+const Parent = styled.div`
+  width: 100%;
+  height: 400px;
+  display: flex;
+`;
+
+const Container = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+
+  ${Image}:hover {
+    opacity: .7;
+  }
+`;
+
+const SmallContainer = styled.div`
+  flex-wrap: wrap;
+  min-width: 50%;
+  height: 50%;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -11,11 +42,20 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.data.collection.map((item) => {
-          return <img src={item.url} alt={item.caption} height="200" width="200w" />;
-        })}
-      </div>
+      <Parent>
+        <Container>
+          <Image />
+          {/* {this.state.data.collection[0]} */}
+        </Container>
+        <Container>
+
+          {/* <Image background-image={this.state.data.collection[1].url} />
+          {this.state.data.collection.map((item) => {
+            return <SmallContainer><Image></Image></SmallContainer>;
+          })} */}
+
+        </Container>
+      </Parent>
     );
   }
 }
