6f60594afee8bb6004366959f19db06b1ad84460
Angela Lee <rubii.kun@gmail.com>
2019-02-13 14:28:35 -0800

    Displays modal mock
    Caption mock in progress

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Modal from './Modal.jsx';
 import { dummyData } from '../../../data';
 
+// How do I make this dynamic?
 const Image = styled.div`
   width: 100%;
   height: 100%;
-  background-image: url(https://news.nationalgeographic.com/content/dam/news/2018/05/17/you-can-train-your-cat/02-cat-training-NationalGeographic_1484324.ngsversion.1526587209178.adapt.1900.1.jpg);
+  background-image: url(https://cdn.theatlantic.com/assets/media/img/mt/2017/06/shutterstock_319985324/lead_720_405.jpg?mod=1533691890);
   background-size: 100% 100%;
+  cursor: pointer;
 `;
 
 const Parent = styled.div`
   width: 100%;
-  height: 400px;
+  height: 430px;
   display: flex;
 `;
 
@@ -32,30 +35,67 @@ const SmallContainer = styled.div`
   height: 50%;
 `;
 
+const Button = styled.button`
+  position: absolute;
+  top: 390px;
+  right: 24px;
+  padding: 6px 15px 6px 15px;
+  border-radius: 4px;
+  font-size: 14px;
+  color: #484848;
+  cursor: pointer;
+`;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: dummyData[0],
+      // data: dummyData[0],
+      show: false,
     };
+    this.showModal = this.showModal.bind(this);
+  }
+
+  showModal() {
+    this.setState({
+      show: !this.state.show,
+    });
   }
 
   render() {
+    if (this.state.show) {
+      return (<Modal closeModal={this.showModal} show={this.state.show}>Showing</Modal>)
+    }
     return (
-      <Parent>
-        <Container>
-          <Image />
-          {/* {this.state.data.collection[0]} */}
-        </Container>
-        <Container>
+      <div>
+        <Modal closeModal={this.showModal} show={this.state.show}>Message in Modal</Modal>
+        <Parent>
+          <Container>
+            {/* These images should be dynamic */}
+            <Image />
+            {/* {this.state.data.collection[0]} */}
+          </Container>
+          <Container>
 
-          {/* <Image background-image={this.state.data.collection[1].url} />
-          {this.state.data.collection.map((item) => {
+            <SmallContainer>
+              <Image />
+            </SmallContainer>
+            <SmallContainer>
+              <Image />
+            </SmallContainer>
+            <SmallContainer>
+              <Image />
+            </SmallContainer>
+            <SmallContainer>
+              <Image />
+            </SmallContainer>
+            {/* {this.state.data.collection.map((item) => {
             return <SmallContainer><Image></Image></SmallContainer>;
           })} */}
-
-        </Container>
-      </Parent>
+            <Button onClick={this.showModal}>View Photos</Button>
+          </Container>
+        </Parent>
+      </div>
     );
   }
 }
---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Body = styled.div`
+  background: rgba(0, 0, 0, 0.85);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  left: 0px;
+  top: 0px;
+`;
+
+const Container = styled.div`
+width: 100%;
+`;
+
+const TopBar = styled.div`
+height: 86px;
+`;
+
+const Exit = styled.div`
+  right: 0px;
+  height: 50px;
+  width: 50px;
+  font-size: 28px;
+  color: white;
+  float: right;
+  cursor: pointer;
+  margin-top: 13px;
+`;
+
+const BottomBar = styled.div`
+  height: 123px;
+`;
+
+const Photo = styled.div`
+align-content: center;
+margin-left: auto;
+margin-right: auto;
+width: 100%;
+height: 565px;
+
+display: flex;
+`;
+
+const Image = styled.img`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 800px;
+
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+`;
+
+const Arrow = styled.div`
+display: table;
+padding: 3px;
+width 100px;
+color:white;
+cursor: pointer;
+font-size: 42px;
+
+`;
+
+const Right = styled(Arrow)`
+display: table-cell; 
+vertical-align: middle; 
+text-align: center; 
+padding-top: 280px;
+
+`;
+
+const Left = styled(Arrow)`
+display: table-cell; 
+vertical-align: middle; 
+text-align: center; 
+padding-top: 280px;
+
+`;
+
+class Modal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  closeModal(event) {
+    this.props.closeModal && this.props.closeModal(event);
+  }
+
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div>
+        <Body>
+          <Container>
+            <TopBar><Exit>X</Exit></TopBar>
+            <Photo>
+              <Left>L</Left>
+              {/* <Arrow><Right /></Arrow>
+              {this.props.children}
+              <div>
+                <button type="button" closeModal={this.closeModal}>Close</button>
+              </div>
+              <Arrow><Right /></Arrow> */}
+              <Image src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80.jpg" alt="" />
+              <Right> R</Right>
+            </Photo>
+            <BottomBar>Caption here</BottomBar>
+          </Container>
+        </Body>
+      </div>
+    );
+  }
+}
+
+Modal.propTypes = {
+  closeModal: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
+};
+
+export default Modal;
