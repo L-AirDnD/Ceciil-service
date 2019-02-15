86161eb3dd6e826481b2dd74086ad0f358991877
Angela Lee <rubii.kun@gmail.com>
2019-02-15 13:33:14 -0800

    WIP connection between client, server, and db

--- database/index.js
+++ database/index.js
@@ -17,4 +17,27 @@ const offeringSchema = new Schema({
   photos: [{ url: String, caption: String }],
 });
 
-const Offering = mongoose.model('Offerings_Photos', offeringSchema);
+const Offering = mongoose.model('Offerings_Photo', offeringSchema);
+
+// Refactor to filter by id
+const retrieve = (err, cb) => {
+  // console.log(id);
+  if (err) {
+    console.log('There was an error in retrieve: ', err);
+    return;
+  }
+  Offering.find({ offering_id: 1003 })
+    .exec((err, data) => {
+      console.log('This is the data in database:', data)
+      if (err) {
+        console.log('There was an error in query: ', err);
+        return;
+      }
+      cb(data);
+    });
+};
+
+module.exports = {
+  Offering,
+  retrieve,
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('../database/index.js');
 
 const app = express();
-
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.get('/api/photos/:offering', (req, res) => {
+  // I expect this to show in my terminal
+  console.log('You pinged the server with Postman!');
+
+  // Manually add the offering_id here
+  let id = 1036;
+  db.retrieve(null, (data) => {
+    console.log('Data from database: ', data);
+    res.status(200).send(data);
+  });
+});
+
+// app.get('/api/photos/:offering', (req, res) => {
+//   db.retrieve(null, (data) =>{
+//     console.log(data);
+//     res.send(data);
+//   })
+// });
 
 app.listen(port, () => {
   console.log(`Listening to port: ${port}`);
