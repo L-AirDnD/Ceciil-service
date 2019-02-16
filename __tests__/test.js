24104b21a14d2c81878f7b5620dd120778208e3a
Angela Lee <rubii.kun@gmail.com>
2019-02-15 23:04:33 -0800

    WIP Jest/Enzyme tests
    Working on Modal tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import Enzyme, { mount, render, shallow} from 'enzyme';
 import App from '../client/src/components/App.jsx';
+import Modal from '../client/src/components/Modal.jsx';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter});
 
-describe('This is a test', () => {
-  test('renders', () => {
-    const a = shallow(<App />);
-    expect(a.exists()).toBe(true);
+describe('App', () => {
+  test('should render', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+  test('should not show modal upon rendering', () => {
+    const wrapper = shallow(<App />);
+    const showState = wrapper.state().show;
+    expect(showState).toEqual(false);
+  });
+});
+
+describe('Modal', () => {
+  test('should render modal on button click', () => {
+    const wrapper = mount(<App />);
+    wrapper
+      .find('button')
+      .simulate('click');
+    expect(wrapper).toMatchSnapshot();
+    wrapper.unmount;
+  });
+  test('should render previous photo by clicking L', () => {
+    const wrapper = shallow(<Modal />);
+    const left = wrapper.find('Left');
+    left
+      .simulate('click')
+    expect(left).to.have.length(1);
+    expect(left).toMatchSnapshot();
+  })
+  test('should render next photo by clicking R', () => {
+    const wrapper = shallow(<Modal />);
+    const right = wrapper.find('Right');
+    right
+      .simulate('click')
+    expect(right).to.have.length(1);
+    expect(right).toMatchSnapshot();
   })
 });
