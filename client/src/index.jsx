601c0d6f79fc510cc6f1580147f5b799e3e4d33f
Angela Lee <rubii.kun@gmail.com>
2019-02-19 16:39:45 -0800

    Update id from app -> photos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Photos from './components/App.jsx';
 
-ReactDOM.render(<Photos />, document.getElementById('app'));
+ReactDOM.render(<Photos />, document.getElementById('photos'));
